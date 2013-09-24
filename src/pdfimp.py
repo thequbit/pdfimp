37ed0b88c6e2af5fba4c8beb47ff53a6579df132
thequbit <thequbit@gmail.com>
2013-09-23 20:08:12 -0400

    realized that i needed to go through the entire site ...

--- src/pdfimp.py
+++ src/pdfimp.py
@@ -13,15 +13,15 @@ class pdfimp:
     _processed = []
     _pdfs = []
 
-    def __init__(self):
-        self._verbose = True
+    def __init__(self,verbose):
+        self._verbose = verbose
 
     def _nonascii(self,s):
         return "".join(i for i in s if ord(i)<128)
 
     def _report(self,text):
         if self._verbose == True:
-            print "[INFO   ] {0}".format(text)
+            print "[PDFImp  ] {0}".format(text)
    
     def _createlink(self,siteurl,link):
         if ( (len(link) >= 7 and link[0:7].lower() == "http://") or
@@ -76,7 +76,7 @@ class pdfimp:
             for _link in links:
                 link,linktext = _link
                 if link in self._processed:
-                    print "Link already processed. '{0}'".format(link)
+                    #print "Link already processed. '{0}'".format(link)
                     continue
 
                 ignored = 0            
@@ -85,40 +85,54 @@ class pdfimp:
                 pagelinks = self._getpagelinks(siteurl,link)
                 _m,_l = self._createlink(siteurl,link)
                 self._processed.append(_l)
-                self._report("Found {0} links ...".format(len(pagelinks)))
+                self._report("Processing {0} links ...".format(len(pagelinks)))
 
+                #
+                # Look at the links found on the page, and add those that are within the domain to 'thelinks'
+                #
                 thelinks = []
                 for _pagelink in pagelinks:
                     match,pagelink,linktext = _pagelink
                     if( match == True ): #and ( (level != maxlevel) or (level == maxlevel and (not pagelink in retlinks) ) ) ):
                         thelinks.append((pagelink,linktext))
-                for l in thelinks:
-                    if level >= maxlevel:
-                        self._processed.append(l)
+                #for l in thelinks:
+                #    if level >= maxlevel:
+                #        self._processed.append(l)
                 
+                #
+                # Follow all of the link within the 'thelink' array
+                #
                 gotlinks = self._followlinks(maxlevel=maxlevel,siteurl=siteurl,links=thelinks,level=level,filesize=filesize,verbose=verbose)
                 for _gotlink in gotlinks:
                     gotlink,linktext = _gotlink
+                    
+                    # only process if we haven't processed it
                     if not any(gotlink in r for r in retlinks):
                         success,linktype = self._typelink(gotlink,filesize)
-                        if success == True and linktype == 'application/pdf' and not gotlink in self._processed:
+                        
+                        # if it is a pdf, then add it to the self._pdfs array
+                        if success == True and linktype == 'application/pdf': # and not gotlink in self._processed:
                             #retlinks.append((gotlink,linktext))
                             self._pdfs.append((gotlink,linktext))
                             self._processed.append(gotlink)
-                            self._report("L = {0}, C = {1}, Added '{2}'".format(level,len(retlinks),gotlink))
+                            self._report("Found Document: '{0}'".format(gotlink))
                         else:
                             ignored += 1
     
+                #
+                # Some of the links that were returned from this page might be pdfs, if they are, add them to the list
+                # of pdfs to be returned 'retlinks'
+                #
                 for _thelink in thelinks:
                    thelink,linktext = _thelink 
                    if not any(thelink in r for r in retlinks):
                         success,linktype = self._typelink(thelink,filesize)
-                        if success == True and linktype == 'application/pdf' and not thelink in self._processed:
+                        if success == True and linktype == 'application/pdf': # and not thelink in self._processed:
                             self._pdfs.append((thelink,linktext))
                             retlinks.append((thelink,linktext))
                             self._processed.append(thelink)
                             #self._report("Added '{0}'".format(thelink))
-                            self._report("L = {0}, C = {1}, Added '{2}'".format(level,len(retlinks),thelink))
+                            self._report("Found Document: '{0}'".format(thelink))
                         else:
                             ignored += 1
     
